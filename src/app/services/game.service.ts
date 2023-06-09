7afd9727aac9fb330fcda5deef22013a08cdc5f0
Julien SANCHEZ <chadow.video@gmail.com>
2023-06-09 23:17:41 +0200

    feat : create game Service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {API_URL} from "./config";
+import {GameCreate} from "../models/game.model";
+import {ResponseAPI} from "../models/responseAPI.model";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  gameUrl: string = `${API_URL}/game`;
+
+  public createGame(game: GameCreate) {
+    return this.http.post<ResponseAPI>(this.gameUrl + "/create", game);
+  }
+
+  public deleteGame(gameId: string): Observable<ResponseAPI> {
+    return this.http.delete<ResponseAPI>(this.gameUrl + "/" + gameId);
+  }
+
+  public updateGame(gameId: string, game: GameCreate): Observable<ResponseAPI> {
+    console.log(gameId);
+    return this.http.put<ResponseAPI>(this.gameUrl + "/" + gameId, game);
+  }
+
+  public getGameInformation(gameId: string | null): Observable<ResponseAPI> {
+    return this.http.get<ResponseAPI>(this.gameUrl + "/" + gameId);
+  }
+
+  public startGame(gameId: string | null): Observable<ResponseAPI> {
+    return this.http.get<ResponseAPI>(this.gameUrl + "/" + gameId + "/start")
+  }
+
+  public getStatusText(status: number): string {
+    switch (status) {
+      case 0:
+        return 'non commencé';
+      case 1:
+        return 'en cours';
+      case 2:
+        return 'terminé';
+      default:
+        return 'unknown';
+    }
+  }
 }
