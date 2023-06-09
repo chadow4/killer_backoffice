f2fe9df602449dc49f62d9ecb4ab94defb900b37
Julien SANCHEZ <chadow.video@gmail.com>
2023-06-09 22:39:21 +0200

    feat: create differents pages with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {HomeComponent} from "./components/pages/home/home.component";
+import {LoginComponent} from "./components/pages/login/login.component";
+import {RegisterComponent} from "./components/pages/register/register.component";
+import {DashboardComponent} from "./components/pages/dashboard/dashboard.component";
+import {CreateGameComponent} from "./components/pages/create-game/create-game.component";
+import {CreateUsersComponent} from "./components/pages/create-users/create-users.component";
+import {EditGameComponent} from "./components/pages/edit-game/edit-game.component";
 
 const routes: Routes = [
-
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home',component: HomeComponent},
+  {path: 'login', component: LoginComponent, canActivate: []},
+  {path: 'register', component: RegisterComponent, canActivate: []},
+  {path: 'dashboard', component: DashboardComponent, canActivate: []},
+  {path: 'create-game', component: CreateGameComponent, canActivate: []},
+  {path: 'create-users', component: CreateUsersComponent, canActivate: []},
+  {path: 'edit-game/:game_name/:idgame', component: EditGameComponent, canActivate: []},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,24 @@ import {RouterModule} from "@angular/router";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AppRoutingModule} from "./app-routing.module";
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { CreateGameComponent } from './components/pages/create-game/create-game.component';
+import { CreateUsersComponent } from './components/pages/create-users/create-users.component';
+import { EditGameComponent } from './components/pages/edit-game/edit-game.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    CreateGameComponent,
+    CreateUsersComponent,
+    EditGameComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
